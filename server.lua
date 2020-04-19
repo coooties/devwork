4eecb88d55b645b5c73c1cab368e21319b20448a
coooties <63178446+coooties@users.noreply.github.com>
2020-04-18 23:49:10 -0500

    Created server.lua for storeRobbery

---
+++ server.lua
@@ -0,0 +1,409 @@
+robbables = {
+    ["Little Seoul 24/7 Cash Register 1"] = {
+        name = "Little Seoul 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Little Seoul 24/7 Cash Register 2"] = {
+        name = "Little Seoul 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Little Seoul 24/7 Safe"] = {
+        name = "Little Seoul 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Innoncence Blvd 24/7 Cash Register 1"] = {
+        name = "Innoncence Blvd 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Innoncence Blvd 24/7 Cash Register 2"] = {
+        name = "Innoncence Blvd 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Innoncence Blvd 24/7 Safe"] = {
+        name = "Innoncence Blvd 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Davis Ave 24/7 Cash Register 1"] = {
+        name = "Davis Ave 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Davis Ave 24/7 Cash Register 2"] = {
+        name = "Davis Ave 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Davis Ave 24/7 Safe"] = {
+        name = "Davis Ave 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Clinton Ave 24/7 Cash Register 1"] = {
+        name = "Clinton Ave 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Clinton Ave 24/7 Cash Register 2"] = {
+        name = "Clinton Ave 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Clinton Ave 24/7 Safe"] = {
+        name = "Clinton Ave 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Prosperity St Robs Liquor Cash Register 1"] = {
+        name = "Prosperity St Robs Liquor Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Prosperity St Robs Liquor Safe"] = {
+        name = "Prosperity St Robs Liquor Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Great Ocean Hwy Robs Liquor Cash Register 1"] = {
+        name = "Great Ocean Hwy Robs Liquor Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Great Ocean Hwy Robs Liquor Safe"] = {
+        name = "Great Ocean Hwy Robs Liquor Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Barbareno Rd 24/7 Cash Register 1"] = {
+        name = "Barbareno Rd 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Barbareno Rd 24/7 Cash Register 2"] = {
+        name = "Barbareno Rd 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Barbareno Rd 24/7 Safe"] = {
+        name = "Barbareno Rd 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Alhambra Dr 24/7 Cash Register 1"] = {
+        name = "Alhambra Dr 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Alhambra Dr 24/7 Cash Register 2"] = {
+        name = "Alhambra Dr 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Alhambra Dr 24/7 Safe"] = {
+        name = "Alhambra Dr 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["RT 68 Robs Liquor Cash Register 1"] = {
+        name = "RT 68 Robs Liquor Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["RT 68 Robs Liquor Cash Register 2"] = {
+        name = "RT 68 Robs Liquor Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["RT 68 Robs Liquor 24/7 Safe"] = {
+        name = "RT 68 Robs Liquor 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Grapeseed Main St 24/7 Cash Register 1"] = {
+        name = "Grapeseed Main St 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Grapeseed Main St 24/7 Cash Register 2"] = {
+        name = "Grapeseed Main St 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Grapeseed Main St 24/7 Safe"] = {
+        name = "Grapeseed Main St 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Senora Fwy 24/7 Cash Register 1"] = {
+        name = "Senora Fwy 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Senora Fwy 24/7 Cash Register 2"] = {
+        name = "Senora Fwy 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Senora Fwy 24/7 Safe"] = {
+        name = "Senora Fwy 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Senora Fwy Sandy 24/7 Cash Register 1"] = {
+        name = "Senora Fwy Sandy 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Senora Fwy Sandy 24/7 Cash Register 2"] = {
+        name = "Senora Fwy Sandy 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Senora Fwy Sandy 24/7 Safe"] = {
+        name = "Senora Fwy Sandy 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["Palomino Fwy 24/7 Cash Register 1"] = {
+        name = "Palomino Fwy 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Palomino Fwy 24/7 Cash Register 2"] = {
+        name = "Palomino Fwy 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Palomino Fwy 24/7 Safe"] = {
+        name = "Palomino Fwy 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["El Rancho Blvd Robs Liquor Cash Register 1"] = {
+        name = "El Rancho Blvd Robs Liquor Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["El Rancho Blvd Robs Liquor Safe"] = {
+        name = "El Rancho Blvd Robs Liquor Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["LTD Gas West Mirror DR Cash Register 1"] = {
+        name = "LTD Gas West Mirror DR Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["LTD Gas West Mirror DR Cash Register 2"] = {
+        name = "LTD Gas West Mirror DR Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["LTD Gas West Mirror DR Safe"] = {
+        name = "LTD Gas West Mirror DR Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["San Andreas Ave Robs Liquor Cash Register 1"] = {
+        name = "San Andreas Ave Robs Liquor Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["San Andreas Ave Robs Liquor Safe"] = {
+        name = "San Andreas Ave Robs Liquor Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["LTD Gas North Rockford DR Cash Register 1"] = {
+        name = "LTD Gas North Rockford DR Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["LTD Gas North Rockford DR Cash Register 2"] = {
+        name = "LTD Gas North Rockford DR Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["LTD Gas North Rockford DR Safe"] = {
+        name = "LTD Gas North Rockford DR Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["RT 68 24/7 Cash Register 1"] = {
+        name = "RT 68 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["RT 68 24/7 Cash Register 2"] = {
+        name = "RT 68 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["RT 68 24/7 Safe"] = {
+        name = "RT 68 24/7 Safe",
+        stockMoney = 700,
+        maxMoney = 1200,
+        moneyRegen = 53, -- per minute
+        type = 2
+    },
+    ["RT 68 Great Ocean 24/7 Cash Register 1"] = {
+        name = "RT 68 Great Ocean 24/7 Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["RT 68 Great Ocean 24/7 Cash Register 2"] = {
+        name = "RT 68 Great Ocean 24/7 Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Don's Country Store Paleto Blvd Cash Register 1"] = {
+        name = "Don's Country Store Paleto Blvd Cash Register 1",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    },
+    ["Don's Country Store Paleto Blvd Cash Register 2"] = {
+        name = "Don's Country Store Paleto Blvd Cash Register 2",
+        stockMoney = 300,
+        maxMoney = 600,
+        moneyRegen = 53, -- per minute
+        type = 1
+    }
+}
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(60000)
+		for k,v in pairs(robbables) do
+			if(v.stockMoney<v.maxMoney)then
+				v.stockMoney = v.stockMoney + v.moneyRegen
+			end
+		end
+	end
+end)
+
+local random = math.random(1, 100) 
+RegisterServerEvent("v_jobs:robberyOver")
+AddEventHandler("v_jobs:robberyOver", function(name)
+    if random > 5 then
+        if robbables[name].type == 2 then 
+        --TriggerClientEvent('fsn_inventory:itemAdd', modified_bit, source, robbables[type]) add the bit item here
+        end
+    end   	
+        TriggerClientEvent('fsn_bank:change:walletAdd', source, robbables[name].stockMoney)
+        robbables[name].stockMoney = 0
+end)
+
+
+RegisterServerEvent("v_jobs:syncRobbery")
+AddEventHandler("v_jobs:syncRobbery", function(spots)
+	TriggerClientEvent("v_jobs:syncRobbery", -1, spots)
+end)
