9ad2249f8b066da16a3f8cece29089fa080f1901
coooties <63178446+coooties@users.noreply.github.com>
2020-05-03 03:32:52 -0500

    storeRobbery client.lua
    added to client where drillbits are given

---
+++ client.lua
@@ -0,0 +1,513 @@
+local robbables = {
+    ["Little Seoul 24/7 Cash Register 1"] = {
+        name = "Little Seoul 24/7 Cash Register 1",
+        x=-706.03717041016, y=-915.42755126953, z=19.215593338013,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Little Seoul 24/7 Cash Register 2"] = {
+        name = "Little Seoul 24/7 Cash Register 2",
+        x=-706.0966796875, y=-913.49053955078, z=19.215593338013,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Little Seoul 24/7 Safe"] = {
+        name = "Little Seoul 24/7 Safe",
+        x=-709.66131591797, y=-904.18121337891, z=19.215612411499,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Innoncence Blvd 24/7 Cash Register 1"] = {
+        name = "Innoncence Blvd 24/7 Cash Register 1",
+        x=24.497854232788, y=-1347.2805175781, z=29.497035980225,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Innoncence Blvd 24/7 Cash Register 2"] = {
+        name = "Innoncence Blvd 24/7 Cash Register 2",
+        x=24.491603851318, y=-1344.958984375, z=29.497035980225,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Innoncence Blvd 24/7 Safe"] = {
+        name = "Innoncence Blvd 24/7 Safe",
+        x=28.161478042603, y=-1339.2298583984, z=29.497035980225,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Davis Ave 24/7 Cash Register 1"] = {
+        name = "Davis Ave 24/7 Cash Register 1",
+        x=-47.912860870361, y=-1759.3132324219, z=29.421012878418,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Davis Ave 24/7 Cash Register 2"] = {
+        name = "Davis Ave 24/7 Cash Register 2",
+        x=-46.756553649902, y=-1757.9239501953, z=29.421012878418,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Davis Ave 24/7 Safe"] = {
+        name = "Davis Ave 24/7 Safe",
+        x=-43.43278503418, y=-1748.3061523438, z=29.421014785767,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Clinton Ave 24/7 Cash Register 1"] = {
+        name = "Clinton Ave 24/7 Cash Register 1",
+        x=372.59274291992, y=326.41857910156, z=103.56641387939,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Clinton Ave 24/7 Cash Register 2"] = {
+        name = "Clinton Ave 24/7 Cash Register 2",
+        x=373.05807495117, y=328.73522949219, z=103.56641387939,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Clinton Ave 24/7 Safe"] = {
+        name = "Clinton Ave 24/7 Safe",
+        x=378.19857788086, y=333.3713684082, z=103.56641387939,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Prosperity St Robs Liquor Cash Register 1"] = {
+        name = "Prosperity St Robs Liquor Cash Register 1",
+        x=-1486.2886962891, y=-378.03118896484, z=40.163433074951,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Prosperity St Robs Liquor Safe"] = {
+        name = "Prosperity St Robs Liquor Safe",
+        x=-1478.9509277344, y=-375.41152954102, z=39.163372039795,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Great Ocean Hwy Robs Liquor Cash Register 1"] = {
+        name = "Great Ocean Hwy Robs Liquor Cash Register 1",
+        x=-2966.4155273438, y=390.86743164063, z=15.043312072754,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Great Ocean Hwy Robs Liquor Safe"] = {
+        name = "Great Ocean Hwy Robs Liquor Safe",
+        x=-2959.6228027344, y = 387.05850219727, z = 14.043290138245,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Barbareno Rd 24/7 Cash Register 1"] = {
+        name = "Barbareno Rd 24/7 Cash Register 1",
+        x=-3242.3420410156, y=999.96276855469, z=12.830714225769,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Barbareno Rd 24/7 Cash Register 2"] = {
+        name = "Barbareno Rd 24/7 Cash Register 2",
+        x=-3244.537109375, y=1000.201965332, z=12.830714225769,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Barbareno Rd 24/7 Safe"] = {
+        name = "Barbareno Rd 24/7 Safe",
+        x=-3250.0964355469, y=1004.3968505859, z=12.830713272095,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Alhambra Dr 24/7 Cash Register 1"] = {
+        name = "Alhambra Dr 24/7 Cash Register 1",
+        x=1960.0858154297, y=3740.0727539063, z=32.343765258789,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Alhambra Dr 24/7 Cash Register 2"] = {
+        name = "Alhambra Dr 24/7 Cash Register 2",
+        x=1958.9104003906, y=3742.0295410156, z=32.343753814697,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Alhambra Dr 24/7 Safe"] = {
+        name = "Alhambra Dr 24/7 Safe",
+        x=1959.2678222656, y=3748.9765625, z=32.343753814697,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["RT 68 Robs Liquor Cash Register 1"] = {
+        name = "RT 68 Robs Liquor Cash Register 1",
+        x=1165.9134521484, y=2710.7768554688, z=38.157691955566,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["RT 68 Robs Liquor Safe"] = {
+        name = "RT 68 Robs Liquor Safe",
+        x=1169.2318115234, y=2717.8664550781, z=37.157680511475,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Grapeseed Main St 24/7 Cash Register 1"] = {
+        name = "Grapeseed Main St 24/7 Cash Register 1",
+        x=1696.6066894531, y=4923.9838867188, z=42.063652038574,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Grapeseed Main St 24/7 Cash Register 2"] = {
+        name = "Grapeseed Main St 24/7 Cash Register 2",
+        x=1698.1241455078, y=4922.916015625, z=42.063652038574,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Grapeseed Main St 24/7 Safe"] = {
+        name = "Grapeseed Main St 24/7 Safe",
+        x=1707.9069824219, y=4920.4155273438, z=42.063652038574,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Senora Fwy 24/7 Cash Register 1"] = {
+        name = "Senora Fwy 24/7 Cash Register 1",
+        x=1727.8900146484, y=6415.2119140625, z=35.037265777588,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Senora Fwy 24/7 Cash Register 2"] = {
+        name = "Senora Fwy 24/7 Cash Register 2",
+        x=1728.9205322266, y=6417.294921875, z=35.037265777588,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Senora Fwy 24/7 Safe"] = {
+        name = "Senora Fwy 24/7 Safe",
+        x=1734.8602294922, y=6420.8374023438, z=35.037265777588,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Senora Fwy Sandy 24/7 Cash Register 1"] = {
+        name = "Senora Fwy Sandy 24/7 Cash Register 1",
+        x=2677.9877929688, y=3279.3754882813, z=55.241146087646,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Senora Fwy Sandy 24/7 Cash Register 2"] = {
+        name = "Senora Fwy Sandy 24/7 Cash Register 2",
+        x=2676.0063476563, y=3280.5173339844, z=55.241146087646,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Senora Fwy Sandy 24/7 Safe"] = {
+        name = "Senora Fwy Sandy 24/7 Safe",
+        x=2672.7150878906, y=3286.6567382813, z=55.241146087646,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["Palomino Fwy 24/7 Cash Register 1"] = {
+        name = "Palomino Fwy 24/7 Cash Register 1",
+        x=2557.2263183594, y=380.74984741211, z=108.62303161621,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Palomino Fwy 24/7 Cash Register 2"] = {
+        name = "Palomino Fwy 24/7 Cash Register 2",
+        x=2554.8825683594, y=380.92050170898, z=108.62303161621,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Palomino Fwy 24/7 Safe"] = {
+        name = "Palomino Fwy 24/7 Safe",
+        x=2549.1870117188, y=384.87322998047, z=108.62303161621,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["El Rancho Blvd Robs Liquor Cash Register 1"] = {
+        name = "El Rancho Blvd Robs Liquor Cash Register 1",
+        x=1134.232421875, y=-982.44580078125, z=46.415840148926,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["El Rancho Blvd Robs Liquor Safe"] = {
+        name = "El Rancho Blvd Robs Liquor Safe",
+        x=1126.7368164063, y=-980.09436035156, z=45.41580581665,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["LTD Gas West Mirror DR Cash Register 1"] = {
+        name = "LTD Gas West Mirror DR Cash Register 1",
+        x=1164.9838867188, y=-324.49517822266, z=69.205146789551,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["LTD Gas West Mirror DR Cash Register 2"] = {
+        name = "LTD Gas West Mirror DR Cash Register 2",
+        x=1164.6693115234, y=-322.62936401367, z=69.205146789551,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["LTD Gas West Mirror DR Safe"] = {
+        name = "LTD Gas West Mirror DR Safe",
+        x=1159.45703125, y=-314.05053710938, z=69.205146789551,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["San Andreas Ave Robs Liquor Cash Register 1"] = {
+        name = "San Andreas Ave Robs Liquor Cash Register 1",
+        x=-1221.9484863281, y=-908.25817871094, z=12.326356887817,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["San Andreas Ave Robs Liquor Safe"] = {
+        name = "San Andreas Ave Robs Liquor Safe",
+        x=-1220.8951416016, y=-916.01702880859, z=11.326335906982,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["LTD Gas North Rockford DR Cash Register 1"] = {
+        name = "LTD Gas North Rockford DR Cash Register 1",
+        x=-1818.9428710938, y=792.89263916016, z=138.08305358887,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["LTD Gas North Rockford DR Cash Register 2"] = {
+        name = "LTD Gas North Rockford DR Cash Register 2",
+        x=-1820.2034912109, y=794.23303222656, z=138.08985900879,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["LTD Gas North Rockford DR Safe"] = {
+        name = "LTD Gas North Rockford DR Safe",
+        x=-1829.2579345703, y=798.76483154297, z=138.19233703613,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["RT 68 24/7 Cash Register 1"] = {
+        name = "RT 68 24/7 Cash Register 1",
+        x=549.03540039063, y=2671.3337402344, z=42.156509399414,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["RT 68 24/7 Cash Register 2"] = {
+        name = "RT 68 24/7 Cash Register 2",
+        x=549.33551025391, y=2669.0808105469, z=42.156509399414,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["RT 68 24/7 Safe"] = {
+        name = "RT 68 24/7 Safe",
+        x=546.41778564453, y=2662.8139648438, z=42.156509399414,
+        beingRobbed = false,
+        robTime = 120,
+        register = false,
+    },
+    ["RT 68 Great Ocean 24/7 Cash Register 1"] = {
+        name = "RT 68 Great Ocean 24/7 Cash Register 1",
+        x=-2539.2592773438, y=2313.8544921875, z=33.410846710205,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["RT 68 Great Ocean 24/7 Cash Register 2"] = {
+        name = "RT 68 Great Ocean 24/7 Cash Register 2",
+        x=-2539.1916503906, y=2311.5480957031, z=33.410820007324,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Don's Country Store Paleto Blvd Cash Register 1"] = {
+        name = "Don's Country Store Paleto Blvd Cash Register 1",
+        x=160.53262329102, y=6641.5854492188, z=31.710643768311,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    },
+    ["Don's Country Store Paleto Blvd Cash Register 2"] = {
+        name = "Don's Country Store Paleto Blvd Cash Register 2",
+        x=162.17066955566, y=6643.2514648438, z=31.710643768311,
+        beingRobbed = false,
+        robTime = 60,
+        register = true,
+    }
+}
+
+local robbing = false
+local robbingObject = nil
+
+Citizen.CreateThread(function()
+    while true do
+     --   Citizen.Wait(0)
+		local pos = GetEntityCoords(GetPlayerPed(-1), true)
+        for k, v in pairs(robbables) do
+            if (Vdist(pos.x, pos.y, pos.z, v.x, v.y, v.z) < 5) and not robbing and not v.beingRobbed and exports["fsn_inventory"]:fsn_HasItem('lockpick') and exports["fsn_inventory"]:fsn_GetItemAmount("lockpick") >= 1  and exports['fsn_police']:fsn_getCopAmt() >= 2 then
+                DrawMarker(25, v.x, v.y, v.z-1, 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 0.5001, 169, 169, 169, 100, 0, 0, 0, 0)
+                if (Vdist(pos.x, pos.y, pos.z, v.x, v.y, v.z) < 1.5) then
+                    if v.register then
+                        SetTextComponentFormat("STRING")
+                        AddTextComponentString("[E] to start prying open the register.")
+                        DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+                    else
+                        SetTextComponentFormat("STRING")
+                        AddTextComponentString("[E] to start cracking the safe.")
+                        DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+                    end
+                    if IsControlJustPressed(1, 38) then
+                        TriggerEvent('v_jobs:StartRobbery')
+                        robbingObject = v
+                    end
+                else
+                    if robbing then
+                        StopRobbery()
+                    end
+                end
+            end
+        end
+
+        if (IsControlJustPressed(1, 38) or IsControlJustPressed(1, 48) or IsControlJustPressed(1, 288)) and robbing then
+            StopRobbery()
+        end
+
+        Citizen.Wait(0)
+    end
+end)
+
+RegisterNetEvent("v_jobs:StartRobbery")
+AddEventHandler("v_jobs:StartRobbery", function()
+    local currentRobberies = 0
+    for i,v in ipairs(robbables) do
+        if v.beingRobbed then
+            currentRobberies = currentRobberies + 1
+        end
+    end
+    while not HasAnimDictLoaded("mini@safe_cracking") do
+        RequestAnimDict("mini@safe_cracking")
+        Citizen.Wait(5)
+    end
+
+    Citizen.CreateThread(function()
+    --    local callChance = math.random(1, 100)
+    --    if callChance > 10 then
+            local pos = GetEntityCoords(GetPlayerPed(-1))
+            local coords = {
+            x = pos.x,
+            y = pos.y,
+            z = pos.z
+            }
+            --TriggerServerEvent('fsn_police:dispatch', coords, 12)
+   --     end
+   --     TaskPlayAnim(GetPlayerPed(-1),"mini@repair","fixing_a_player", 8.0, 0.0, -1, 1, 0, 0, 0, 0)    
+        robbing = true
+        robbables[robbingObject.name]=robbingObject
+        TriggerServerEvent("v_jobs:syncRobbery", robbables)
+     --   FreezeEntityPosition(GetPlayerPed(-1), true)
+        local seconds = 0
+        Citizen.CreateThread(function()
+            while robbing do
+                if robbingObject.register then
+                    SetTextComponentFormat("STRING")
+                    AddTextComponentString("You're prying open the register! ("..robbingObject.robTime - seconds.." seconds left)")
+                    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+                    if not IsEntityPlayingAnim(GetPlayerPed(-1), "mini@safe_cracking", "dial_turn_clock_fast", 3) then
+                        TaskPlayAnim(GetPlayerPed(-1), "mini@safe_cracking", 'dial_turn_clock_fast', 30.0, 30.0, 1.0, 1, 0.0, 0, 0, 0)
+                    end
+                    exports["vt_taskbar"]:barStartActive('lockpicking register',robbingObject.robTime)
+                else
+                    SetTextComponentFormat("STRING")
+                    AddTextComponentString("You're cracking into the safe! ("..robbingObject.robTime - seconds.." seconds left)")
+                    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+                    if not IsEntityPlayingAnim(GetPlayerPed(-1), "mini@safe_cracking", "dial_turn_clock_slow", 3) then
+                        TaskPlayAnim(GetPlayerPed(-1), "mini@safe_cracking", 'dial_turn_clock_slow', 90.0, 90.0, 1.0, 1, 0.0, 0, 0, 0)
+                    end
+                    exports["vt_taskbar"]:barStartActive('cracking safe',robbingObject.robTime)
+                end
+                if GetDistanceBetweenCoords(robbingObject.x, robbingObject.y, robbingObject.z,GetEntityCoords(PlayerPedId())) > 1 then
+                    StopRobbery()
+                end
+                Citizen.Wait(0)
+            end
+        end)
+        while robbing do
+            seconds = seconds + 1
+            if seconds == robbingObject.robTime then
+                RobberyOver()
+            end
+            Citizen.Wait(1000)
+        end
+    end)
+end)
+
+RegisterNetEvent("v_jobs:syncRobbery")
+AddEventHandler("v_jobs:syncRobbery", function(tbl)
+    robbables = tbl
+end)
+
+function StopRobbery()
+    exports['mythic_notify']:DoCustomHudText('success', 'Robbery Stopped/Finished', 4000)
+    exports["vt_taskbar"]:barStop()
+    robbing = false
+    robbingObject.beingRobbed = false
+    robbables[robbingObject.name] = robbingObject
+    TriggerServerEvent("v_jobs:syncRobbery", robbables)
+    robbingObject = nil
+    ClearPedTasksImmediately(GetPlayerPed(-1))
+  --  FreezeEntityPosition(GetPlayerPed(-1), false)
+end
+
+function RobberyOver()
+    TriggerServerEvent("v_jobs:robberyOver", robbingObject.name)
+    TriggerEvent('fsn_inventory:item:take', 'lockpick', 1)
+    local pos = GetEntityCoords(GetPlayerPed(-1), true)
+    for k, v in pairs(robbables) do
+        if (Vdist(pos.x, pos.y, pos.z, v.x, v.y, v.z) < 1.5) then
+            if v.register == false then
+                if math.random(1, 100) > 95 then
+                    TriggerEvent('fsn_inventory:item:add', 'modified_drillbit', 1)
+                
+                end
+            end
+        end
+    end
+    print("Robbery Over")
+    StopRobbery()
+end
